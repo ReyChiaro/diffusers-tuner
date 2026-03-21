e61dc8855282ff3e799a12cddebf8f6f85a20a7b
ReyChiaro <1402850762@qq.com>
2026-03-21 20:15:20 +0800

    Finish tune loop

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from accelerate.utils import ProjectConfiguration, DistributedDataParallelKwargs
 from accelerate.logging import get_logger
 from hydra.utils import instantiate
 from omegaconf import OmegaConf
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 
-from data_module import DataModule
+from data.data_module import DataModule
 from tuner import Tuner
 
 
@@ -30,21 +30,28 @@ def finetune(cfgs: OmegaConf) -> None:
 
     if is_main_process:
         accelerator.init_trackers(project_name=cfgs.project_name)
-    
-    # ---- Data and Tuner ---- #
-    data_module: DataModule = instantiate(cfgs.data_module)    
-    tuner: Tuner = instantiate(cfgs.tuner)
 
-    # ---- Start Finetune ---- #
-    tuner.finetune(
-        accelerator=accelerator,
-        tune_loader=data_module.tune_loader,
-        eval_loader=data_module.eval_loader,
-        pipe_config=cfgs.pipeline,
-        tune_logger=logger,
+    # ---- Dataset and DataModule ---- #
+    tune_set: Dataset = instantiate(cfgs.tune_set)
+    eval_set: Dataset = instantiate(cfgs.eval_set) if cfgs.eval_set else None
+    data_module: DataModule = instantiate(
+        cfgs.data_module,
+        tune_set=tune_set,
+        eval_set=eval_set,
+        enable_ddp=accelerator.num_processes > 1,
     )
 
-    logger.info("✅ Tuner task finished.")
+    # ---- Start Finetune ---- #
+    Tuner.finetune(
+        accelerator=accelerator,
+        pipe_config=cfgs.pipeline,
+        adpt_config=cfgs.adapter,
+        loss_config=cfgs.loss,
+        tune_config=cfgs.tune,
+        tune_loader=data_module.tune_loader,
+        eval_loader=data_module.eval_loader,
+        tune_logger=logger,
+    )
 
 
 if __name__ == "__main__":
